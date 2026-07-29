2674c857fbd976ea44df9e67fd6ba00d69a462b8
dinzz <dinzzdluffy005@gmail.com>
2026-07-29 22:27:09 +0530

    latest kernel support

--- configuration.nix
+++ configuration.nix
@@ -11,6 +11,7 @@
   nixpkgs.config.allowUnfree = true;
 
   # Use the systemd-boot EFI boot loader.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   
