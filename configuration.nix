d647cd64c50d2802bdcf0e675b948f013ebc70db
dinzz <dinzzdluffy005@gmail.com>
2026-09-12 12:27:25 +0530

    feat: added opencode

--- configuration.nix
+++ configuration.nix
@@ -189,7 +189,7 @@ programs.gamemode.enable = true;
      xwayland-satellite
      mangohud
      protonup-qt
-     impression
+     opencode
      # for dark theme
      adwaita-icon-theme
      gnome-themes-extra
